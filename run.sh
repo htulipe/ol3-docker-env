c22613a9cbc58c1150835cabeab348354d658c95
Hadrien Tulipe <hadrientulipe+dev@gmail.com>
2014-02-04 20:10:23 +0100

    Fix typo in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-sudo docker run -i -p 127.0.0.1:8000:8000 -p 127.0.0.1:9810:9810 -p 127.0.0.1:2200:22 -v $1:/workspace:rw -t htulipe/ol3 /bin/bash
+sudo docker run -i -p 127.0.0.1:8000:8000 -p 127.0.0.1:9810:9810 -p 127.0.0.1:2200:22 -v $1:/workspace:rw -t htipule/ol3 /bin/bash
 
